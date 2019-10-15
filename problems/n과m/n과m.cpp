5171f6dbe46fa346893ca8787f57c5540f0380c1
tre2man <45779423+tre2man@users.noreply.github.com>
2019-10-15 18:47:04 +0900

    1
    1

--- problems/n과m/n과m.cpp
+++ problems/n과m/n과m.cpp
@@ -1,22 +1,35 @@
 #include <stdio.h>
 
-int m,n; //1부터 m까지 중복없이 n개 고른 것  
+int n,m; //1부터 n까지 중복없이 m개 고른 것
+int arr[9]={0};  
 int visit[9]={0};
+
+//그래프 탐색을 이용하여 풀이  
 
 void dfs(int a)
 { 
+	printf("%d start\n",a);
 	if(a==m)
 	{
-		
+		for(int i=0;i<m;i++) printf("%d ",arr[i]);
+		printf("\n");
 	}
-	else
+	
+	for(int i=1;i<=n;i++)
 	{
-		
+		if(visit[i]==0)
+		{
+			visit[i]=1;
+			arr[a]=i;
+			dfs(a+1);
+			visit[i]=0;
+		}
 	}
+	printf("%d end\n",a);
 }
 
 int main()
 {
-	scanf("%d %d",&m,&n);
-	dfs(1);
+	scanf("%d %d",&n,&m);
+	dfs(0);
 }
