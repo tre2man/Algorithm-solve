e31f96af3c0d738de4598f7eba85be64e38cbfc9
tre2man <45779423+tre2man@users.noreply.github.com>
2020-02-10 17:40:47 +0900

    1

--- problems/z/z.cpp
+++ problems/z/z.cpp
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int count=0;
-int n,r,c;
+bool arr[32769][32769]={0};
+int count=0,ans;
 
-int cut(int x,int y,int z)
+
+//Å½»ö ½ÃÀÛ ÁÂÆ÷ x,y Å½»ö Ä­ÀÇ Å©±â z 
+void cut(int x,int y,int z)
 {
-	if(z==2)
+	if(arr[x][y]==1)
 	{
-		
+		printf("%d",count);
+		return;
 	}
 	else
 	{
-		z/=2;
-		cut(x,y,z);
-		cut(x,y+z,z);
-		cut(x+z,y,z);
-		cut(x+z,y+z,z);
+		cut(x,y,z/2);
+		cut(x,y+z/2,z/2);
+		cut(x+z/2,y,z/2);
+		cut(x+z/2,y+z/2,z/2);
 	}
 }
 
 int main()
 {
+	int n,r,c;
 	scanf("%d %d %d",&n,&r,&c);
+	arr[r][c]=1;
 	
 	cut(0,0,(int)pow(2,n));
+	printf("%d",ans);
 }
