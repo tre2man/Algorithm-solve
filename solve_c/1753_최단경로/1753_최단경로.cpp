5d0cf00c767cf4516e1b633947afd29358592da8
tre2man <kimtree97@naver.com>
2021-04-02 00:59:22 +0900

    solved 1753

--- solve_c/1753_최단경로/1753_최단경로.cpp
+++ solve_c/1753_최단경로/1753_최단경로.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 /*
 다익스트라 알고리즘을 이용하여 탐색한다.
+우선순위 큐를 이용하는 알고리즘이다. (누적된 길이의 합이 적은 것부터 시작)
 미완성
 */
 
@@ -17,35 +18,45 @@ typedef struct
 } location;
 
 vector<location> input[300001];
-queue<location> open;
+priority_queue<location> open;
+int max_len[300001];
+int visited[300001];
 int V, E, K, u, v, w;
 
-void dik(int start, int end)
+bool operator<(location a, location b)
 {
-    if (start == end)
+    return (a.len > b.len);
+}
+
+void dijk(int start, int end)
+{
+    /* 시작점 초기비용 */
+    open.push({start, 0});
+    while (!open.empty())
     {
-        printf("0\n");
-        return;
-    }
-    else
-    {
-        open.push({1, 0});
-        while (!open.empty())
+        int start_node = open.top().node;
+        int start_cost = open.top().len;
+        visited[start_node] = 1;
+        open.pop();
+        /* 이웃한 노드의 거리 모두 확인 */
+        for (int i = 0; i < input[start_node].size(); i++)
         {
-            int now_node = open.front().node;
-            int before_len = open.front().len;
-            open.pop();
-            for (int i = 0; i < input[now_node].size(); i++)
+            int next_node = input[start_node][i].node;
+            int next_cost = input[start_node][i].len;
+            /* 최소 경로 발견시? 큐에 삽입 */
+            if (!visited[next_node] && !max_len[next_node] || max_len[next_node] > start_cost + next_cost)
             {
-                int new_len = input[now_node][i].len;
-                int before_len = input[now_node][i].len;
-                /* 길이를 더한 값이 이전 최소 길이보다 작을 경우? 큐에 삽입 */
-                if ()
-                {
-                    
-                }
+                max_len[next_node] = start_cost + next_cost;
+                open.push({next_node, max_len[next_node]});
             }
         }
+    }
+    for (int i = 1; i <= end; i++)
+    {
+        if (i != start && !max_len[i])
+            printf("INF\n");
+        else
+            printf("%d\n", max_len[i]);
     }
 }
 
@@ -55,12 +66,10 @@ int main()
     for (int i = 0; i < E; i++)
     {
         scanf("%d %d %d", &u, &v, &w);
-        location temp;
-        temp.node = v;
-        temp.len = 1;
-        input[u].push_back(temp);
+        input[u].push_back({v, w});
     }
-    for (int i = 1; i <= V; i++)
-        dik(K, i);
+    memset(max_len, 0, sizeof(max_len));
+    memset(visited, 0, sizeof(visited));
+    dijk(K, V);
     return (0);
 }
