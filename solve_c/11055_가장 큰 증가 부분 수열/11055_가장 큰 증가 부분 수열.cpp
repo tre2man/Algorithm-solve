533e1c5318bad99b96cc499cb4c0867e20f17a54
tre2man <kimtree97@naver.com>
2021-04-14 23:26:04 +0900

    11052

--- solve_c/11055_가장 큰 증가 부분 수열/11055_가장 큰 증가 부분 수열.cpp
+++ solve_c/11055_가장 큰 증가 부분 수열/11055_가장 큰 증가 부분 수열.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 using namespace std;
 
+/*
+각 상태에서의 증가 부분 수열(최대 길이)의 합을 저장한다.
+마지막에는 최댓값을 출력한다.
+*/
+
 int main()
 {
     ios::sync_with_stdio(0);
@@ -17,6 +22,7 @@ int main()
         cin >> temp;
         input.push_back(temp);
     }
+    /* 각 상태에서의 최대 길이 증가 부분 수열의 합 입력하기 */
     for (int i = 0; i < N; i++)
     {
         sum.push_back(input[i]);
@@ -26,6 +32,7 @@ int main()
                 sum[i] = max(sum[i], input[i] + sum[j]);
         }
     }
+    /* 최댓값 추려내기 */
     for (int i = 0; i < N; i++)
         if (sum[i] > ans)
             ans = sum[i];
